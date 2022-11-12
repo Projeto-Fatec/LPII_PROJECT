4beab0e3d89cf2a7a9a92f239405e2bd4ad169a7
alexiakarine <alexiakarine18@gmail.com>
2022-11-11 22:35:23 -0300

    alexia

--- src/main/java/controller/ImovelController.java
+++ src/main/java/controller/ImovelController.java
@@ -18,33 +18,23 @@ public class ImovelController {
     ProdutoService produtoService;
 
     @Post("/terreno")
-    public Produto postTerreno(Terreno terreno, Leilao leilao){
+    public Produto postTerreno(Terreno terreno, Leilao leilao) {
         return produtoService.addByLeilao(terreno, leilao);
     }
 
     @Post("/apartamento")
-    public Produto postApartamento(Apartamento apartamento, Leilao leilao){
+    public Produto postApartamento(Apartamento apartamento, Leilao leilao) {
         return produtoService.addByLeilao(apartamento, leilao);
     }
 
     @Post("/casa")
-    public Produto postCasa(Casa casa, Leilao leilao){
+    public Produto postCasa(Casa casa, Leilao leilao) {
         return produtoService.addByLeilao(casa, leilao);
     }
 
     @Post("/edificio-comercial")
-    public Produto postEdificioComercial(EdificioComercial edificioComercial, Leilao leilao){
+    public Produto postEdificioComercial(EdificioComercial edificioComercial, Leilao leilao) {
         return produtoService.addByLeilao(edificioComercial, leilao);
     }
 
-<<<<<<< HEAD
 }
-=======
-    @Post
-    public Imovel addImovel(String nome, String descricao, Double valorInicial, String imovelTipo, Long leilaoId){
-        Imovel imovel = new Imovel(nome, descricao, valorInicial, imovelTipo);
-        Leilao leilao = new Leilao(leilaoId);
-        return imovelService.addImovel(imovel, leilao);
-    }
-}
->>>>>>> a9186a67aedca6356d9878150c3904fb077679a9

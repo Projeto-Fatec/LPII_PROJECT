1727b18242af8be41cb22a35aef1e1858b1bafb1
Zaion Felippe <81268185+ZaionKun@users.noreply.github.com>
2022-11-11 22:26:15 -0300

    fix UI

--- src/test/java/example/micronaut/LeilaoEletronicoTest.java
+++ src/test/java/example/micronaut/LeilaoEletronicoTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import jakarta.inject.Inject;
 
 @MicronautTest
-class MicronautguideTest {
+class LeilaoEletronicoTest {
 
     @Inject
     EmbeddedApplication<?> application;
---
+++ src/test/java/example/micronaut/LeilaoTest.java
@@ -0,0 +1,15 @@
+package example.micronaut;
+
+import org.junit.jupiter.api.Test;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+
+@MicronautTest
+public class LeilaoTest{
+
+    @Test
+    void createLeilaoTest(){
+
+    }
+
+}
